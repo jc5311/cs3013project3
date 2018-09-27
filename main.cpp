2df961c25dbafde04fd1adc70a0e7680b44f9de3
JuanC <jc.psp5@gmail.com>
2018-09-27 17:37:23 -0400

    After much work and no commits. I present the latest update! This bit marks part 1 as basically done. However we need to divide the addition amongst all the threads so that they are not all adding upp the whole range individually but only bits and pieces.

--- main.cpp
+++ main.cpp
@@ -1,106 +1,185 @@
-//Team: Juan Caraballo
-
-//The purpose of this project is to exercise the use of semaphores and mailboxes
-//to build a message passing system that ca be used among a set of threads
-//within the same process.
-
-
-//includes
 #include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
 #include <pthread.h>
 #include <semaphore.h>
 #include <unistd.h>
 
-using namespace std;
-
-//defines
 #define MAXTHREAD 10
 #define RANGE 1
 #define ALLDONE 2
 
-//structs and classes
-class msg{
-public:
-	int iSender; //sender of the message (0 .. numThreads)
-	int type; //message type (RANGE OR ALLDONE)
-	int value1; //first message value
-	int value2; //second message value
+using namespace std;
 
-	msg(){
-		//this constructor was just created for practicing the use of one
-		iSender = 0;
-		type = 0;
-		value1 = 0;
-		value2 = 0;
+//struct
+//structs
+typedef struct{
+
+	int iSender; //sender of message
+	int type; //message type
+	int value1; //first value
+	int value2; //second vlaue
+} msg;
+
+//global vars
+pthread_t tids[MAXTHREAD+1];
+sem_t rcvsem[MAXTHREAD+1];
+sem_t sendsem[MAXTHREAD+1];
+
+msg mailboxes[MAXTHREAD+1];
+int mailbox_ids[MAXTHREAD+1] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+int num_threads = 0;
+int sum_to = 1;
+
+
+//functions
+int SendMsg(int iTo, msg *pMsg){
+	
+	//check if the receive semaphore is available
+	//meaning the mailbox is empty
+	//mutex the action of sending
+	sem_wait(&sendsem[iTo]);
+	mailboxes[iTo] = *pMsg;
+	sem_post(&rcvsem[iTo]);
+	return 0;
+}
+
+int RecvMsg(int iFrom, msg *pMsg){
+	//check if send semaphore is available
+	//meaning mailbox is full
+	sem_wait(&rcvsem[iFrom]);
+	//mutex the action of receiving
+	*pMsg = mailboxes[iFrom];
+	sem_post(&sendsem[iFrom]);
+	return 0;
+}
+
+//thread routines
+void *wthreadRoutine(void *arg){
+	int m_id = *(int*)arg; //lets pass in the mailbox id as an argument
+	msg message;
+	
+	RecvMsg(m_id, &message);
+	int sum = 0;
+	for (int i = message.value1; i <= message.value2; i++){
+		sum += i;
+	}
+	message.iSender = m_id;
+	message.type = ALLDONE;
+	message.value1 = sum;
+	message.value2 = sum;
+	//send ALLDONE message back to parent
+	SendMsg(0, &message);
+	
+	return 0;
+}
+
+void *pthreadRoutine(void *arg){
+	//send messages to all thread mailboxes
+	int sum = 0;
+	int jincrementer;
+	for (int i = 1; i <= num_threads; i++){
+		msg message;
+		message.iSender = 0;
+		message.type = RANGE;
+		message.value1 = 1;
+		message.value2 = sum_to;
+		SendMsg(i, &message);
 	}
 
-	int SendMsg(){
+	//wait for messages from everybody
+	for (int i = 1; i <= num_threads; i++){
+		msg message;
+		RecvMsg(0, &message);
+		if (message.type == ALLDONE){
+			sum += message.value1;
+		}
+	}
+
+	cout << "The total for 1 to " << sum_to << " using " << num_threads << 
+			" threads is " << sum << endl;
+	return 0;
+}
+
+int main(int argc, char *argv[]){
+	//are there two args?
+	//if so, collect their values and properly assign the num_threads we want
+	//and the max number for threads to sum up to
+	if (argc == 3){
+		int nt = atoi(argv[1]);
+		sum_to = atoi(argv[2]);
+		if (nt <= 0){
+			cout << "Error, please enter 1-10 threads as the first argument."
+			<< endl;
+		}
+		else if ((nt > 0) && (nt < MAXTHREAD)){
+			//if nt is between 0 and MAXTHREAD 
+			num_threads = nt;
+		}
+		else{
+			//if nt is >= MAXTHREAD
+			num_threads = MAXTHREAD;
+		}
+	}
+	else{
+		//if two args aren't given, just break the program for now.
 		return 1;
 	}
 
-	int RecvMsg(){
-		return 2;
-	}
-};
-
-//global vars
-static msg mailboxes[MAXTHREAD+1]; //"message = mailbox"; mailboxes
-static sem_t sem_addr[MAXTHREAD+1]; //semaphore addresses for each mailbox
-static pthread_t tids[MAXTHREAD + 1]; //thread tids
-int testval = 1;
-
-
-//thread routine
-void *pt_routine(void *arg){
-	sem_wait(&sem_addr[*(int*)arg]);
-	cout << "My sem was posted to." << endl;
-	return NULL;
-}
-
-
-//main
-int main (int argc, char* argv[]){
-	/*
-	//testing the use of threads
-	cout << "We are here 1." << endl;
-	retval = pthread_create(tid, NULL, pt_routine, (void*)&testval);
-	cout << "pthread_create returned: " << retval << endl;
-	sleep(3);
-	cout << "The new value for testval is: " << testval << endl;
-	*/
-
-	//collect num arg from command line
-	if (argc == 3){
-		int numarg1 = atoi(argv[1]);
-		int numarg2 = atoi(argv[2]);
-		cout << numarg1 << " " << numarg2 << endl;
-	}
-
-	//create 10 semaphores
-	for (int i = 1; i <= MAXTHREAD; i++){
-		int retval = sem_init(&sem_addr[i], 0, 1);
-		if (retval < 0){
+	//create send semaphores
+	for (int i = 0; i <= num_threads; i++){
+		if ( sem_init(&sendsem[i], 0, 1) == 0){
+			//created semaphore successfully...nothing more to do?
+		}
+		else{
 			cerr << "sem_init error";
 		}
 	}
 
-	//create 10 threads and call the pt_thread routine on each
-	for (int i = 1; i <= MAXTHREAD; i++){
-		int retval = pthread_create(&tids[i], NULL, pt_routine, &i);
-		if (retval != 0){
-			cerr << "pthread_create error";
+	//create receive semaphores
+	for (int i = 0; i <= num_threads; i++){
+		if ( sem_init(&rcvsem[i], 0, 0) == 0){
+			//created semaphore successfully...nothing more to do?
+		}
+		else{
+			cerr << "sem_init error";
 		}
 	}
 
-	//signal semaphores
-	sleep (3);
-	for (int i = 1; i <= MAXTHREAD; i++){
-		sem_post(&sem_addr[i]);
-		sleep(1);
+	//create parent thread
+	if (pthread_create(&tids[0], NULL, pthreadRoutine, &mailbox_ids[0]) == 0){
+		//thread created successfully
+	}
+	else{
+		cerr << "pthread_create error";
+		return 1;
+	}
+
+	//create worker threads
+	for (int i = 1; i <= num_threads; i++){
+		if ( pthread_create(&tids[i], NULL, wthreadRoutine, &mailbox_ids[i]) == 0){
+			//thread created successfully...do nothing for now?
+		}
+		else{
+			cerr << "pthread_create error";
+			return 1;
+		}
+	}
+
+	//join threads
+	for (int i = 0; i <= num_threads; i++){
+		int retval;
+		void* res;
+		retval = pthread_join(tids[i], &res);
+		if (retval != 0){
+			cerr << "pthread_join error";
+		}
+	}
+
+	//destroy semaphores
+	for (int i = 0; i <= num_threads; i++){
+		sem_destroy(&sendsem[i]);
+		sem_destroy(&rcvsem[i]);
 	}
 
 
 	return 0;
-} //end of main
+}
